8cc66d42ef0dfc42b4773731dc94ca53f9d38d59
Franco Cabrera <cabrera.f@opendevpro.com>
2021-06-27 18:59:37 -0300

    create validation generation token

--- api-auth/src/main.ts
+++ api-auth/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{ cors: true });
+ // app.use(csurf());
+  app.use(helmet());
+ 
+
   await app.listen(3000);
 }
 bootstrap();
